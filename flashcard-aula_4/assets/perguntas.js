25cfb9b5b24d4fe549783053f47fe920de159c61
.saints. <144707151+NegoCapa01@users.noreply.github.com>
2024-11-11 11:56:20 -0300

    Aula 7

---
+++ flashcard-aula_4/assets/perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'programação'
+    'O que é JavaScript?'
+    'O Javacript é uma limguagem de programação'
+)
+
+criaCartao(
+    'Geografia'
+    'Qual a Capital da França'
+    'A Capital da França é Paris'
+)
+
+criaCartao(
+    'Programação'
+    'O que é uma função'
+    'Uma função é um bloco de código que executa alguma tarefa'
+    categoria,
+    resposta,
+    resposta
+)
